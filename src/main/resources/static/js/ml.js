de9195e9201e5a75bde64b1b6d0f0696fb0ff167
teddie <lj.baer@t-online.de>
2023-05-03 20:44:36 +0200

    added AI

---
+++ src/main/resources/static/js/ml.js
@@ -0,0 +1,29 @@
+const fileInput = document.getElementById('cat');
+const modelURL = "ml/model.json";
+const metadataURL = "ml/metadata.json";
+
+let model;
+
+fileInput.addEventListener('change', async function () {
+    const file = fileInput.files[0];
+    if (!file) return;
+    const url = URL.createObjectURL(file);
+    
+    const imagePreview = document.getElementById('imagePreview');
+    imagePreview.src = url;
+    
+    const prediction = await model.predict(imagePreview);
+    
+    let max = prediction[0];
+    for (let i = 1; i < model.getTotalClasses(); i++) {
+        if (max.probability < prediction[i].probability) {
+            max = prediction[i];
+        }
+    }
+    console.log(max.className);
+    document.getElementById("resu").textContent = max.className;
+});
+
+async function init() {
+    model = await tmImage.load(modelURL, metadataURL);
+}
